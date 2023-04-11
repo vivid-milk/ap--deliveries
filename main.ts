889ce5054189500840d90047271658bf2db3a3b1
vivid-milk <56522561+vivid-milk@users.noreply.github.com>
2023-04-11 08:49:43 -0700

    lots of progress, still bugs with inventory

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 scene.setBackgroundColor(1)
-tiles.setTilemap(tilemap`level1`)
 let car = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -18,46 +17,79 @@ let car = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-    let inventory: color[] = []
 controller.moveSprite(car)
 scene.cameraFollowSprite(car)
-tiles.placeOnTile(car, tiles.getTileLocation(4, 5))
+let inventory: color[] = []
+let housesinfo: number[][] = []
+let giftsinfo: number[][] = []
+let winningScore = 0 // when the players score is equal to this number then the round ends
+let currentLevel = 1
 
-//level 1 hard coding
-let houseLocation: number[] = []
-let giftLocation: number[] = []
+setLevel(currentLevel)
+
+//hard coding of levels instead of random locations
+//column then row then color
+function setLevel(level: number){
+    info.setScore(0)
+    if(level == 1){
+        car.setPosition(64,44)
+        tiles.setTilemap(tilemap`level1`)
+        housesinfo[0] = [2,5,2]
+        giftsinfo[0] = [5,4,2] // one gift
+        winningScore = 1
+    } else if(level == 2){
+        tiles.placeOnTile(car,tiles.getTileLocation(5,5))
+        tiles.setTilemap(tilemap`level2`)
+        housesinfo[0] = [8, 9, 2] 
+        housesinfo[1] = [2, 9, 5]
+        giftsinfo[0] = [2, 6, 2] // two gifts
+        giftsinfo[1] = [6, 13, 5]
+        winningScore = 2
+    }
+}
 
 
-
+let surroundings: number[] = [] // stores if there is a house/gift and then the color of it
 game.onUpdate(function(){
     if (controller.A.isPressed()){
-        if (tiles.tileAtLocationEquals(tiles.getTileLocation(car.tilemapLocation().column + 1, car.tilemapLocation().row), img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . f f . . . . f f . . . . 
-. . . f 1 1 f . . f 1 1 f . . . 
-. . . f 1 1 1 f f 1 1 1 f . . . 
-. . . f f 1 1 f f 1 1 f f . . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . f 2 2 1 1 2 2 1 1 2 2 f . . 
-. . . f f f f f f f f f f . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-        `)) {
-            inventory.push(2)
-            tiles.setTileAt(tiles.getTileLocation(car.tilemapLocation().column + 1, car.tilemapLocation().row), img`.`)
+        surroundings = checkSurroundingTiles(car)
+        if (surroundings[0] == 1) { // check for gifts
+            inventory.push(surroundings[1])
+            tiles.setTileAt(tiles.getTileLocation(giftsinfo[0][0], giftsinfo[0][1]), img`.`) // need to make dynamic
+            giftsinfo[0] = [-10,-10]
         }
-        if()
+        if(surroundings[0] == 0){ // check for houses
+            for (let i = 0; i < 3; i++) {
+                if(inventory[i] == surroundings[1]){
+                    inventory[0] = null
+                    info.changeScoreBy(1)
+                }
+            }
+        }
+    }
+    if(info.score() == winningScore){
+        game.splash("level" + currentLevel + "Complete!")
+        setLevel(currentLevel + 1)
     }
 })
+
+function checkSurroundingTiles(sprite: Sprite){ // checks around sprite for gift or house
+    //sequencing. if was in other order would quit code when detected house so in this method detects gift then picks up gift then on next calling would detect house 
+    if (sprite.tilemapLocation().column == giftsinfo[0][0] || sprite.tilemapLocation().column == giftsinfo[0][0] + 1 || sprite.tilemapLocation().column == giftsinfo[0][0] - 1) {
+        if (sprite.tilemapLocation().row == giftsinfo[0][1] || sprite.tilemapLocation().row == giftsinfo[0][1] + 1 || sprite.tilemapLocation().row == giftsinfo[0][1] - 1) {
+            return [1, giftsinfo[0][2]]
+        }
+    }
+    if (sprite.tilemapLocation().column == housesinfo[0][0] || sprite.tilemapLocation().column == housesinfo[0][0] + 1 || sprite.tilemapLocation().column == housesinfo[0][0] - 1) {
+        if (sprite.tilemapLocation().row == housesinfo[0][1] || sprite.tilemapLocation().row == housesinfo[0][1] + 1 || sprite.tilemapLocation().row == housesinfo[0][1] - 1) {
+            return [0, housesinfo[0][2]]
+        }
+    }
+    return [-1, 16] // 16 is the color-code for black
+}
 scene.createRenderable(scene.HUD_Z, function(target: Image, camera: scene.Camera){
     screen.fillRect(0,110,30,10,6)
     screen.fillRect(2,112,6,6,inventory[0])
     screen.fillRect(12, 112, 6, 6, inventory[1])
-    screen.fillRect(22, 112, 6, 6, inventory[1])
+    screen.fillRect(22, 112, 6, 6, inventory[2])
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -6,6 +6,12 @@ namespace myTiles {
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -28,6 +34,25 @@ namespace myTiles {
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn1,sprites.vehicle.roadHorizontal,sprites.vehicle.roadIntersection3,sprites.vehicle.roadIntersection2,sprites.vehicle.roadTurn3,sprites.vehicle.roadIntersection1,sprites.vehicle.roadTurn4,sprites.vehicle.roadTurn2,sprites.vehicle.roadIntersection4], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002030303030a00000000000002030303040000000001000000000000010500000100000000010000000000000100000001000000000100000000000001000000010000000001000000000000010c000001000006000100000000000007030b030903030b0308000000000000000001000000000100000000000000000000070303030308000000000000000000000000000d0000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+2 . . . . . 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 . 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 . 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 . 2 2 2 2 . 2 2 2 2 2 
+2 . 2 2 2 . 2 2 2 2 . 2 2 2 2 2 
+2 . . . . . . . . . . 2 2 2 2 2 
+2 2 2 . 2 2 2 2 . 2 2 2 2 2 2 2 
+2 2 2 . . . . . . 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn1,sprites.vehicle.roadHorizontal,sprites.vehicle.roadIntersection4,myTiles.tile2,myTiles.tile1,sprites.vehicle.roadTurn3,sprites.vehicle.roadTurn4,sprites.vehicle.roadIntersection1,sprites.vehicle.roadTurn2,sprites.vehicle.roadIntersection3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
         }
         return null;
     })
@@ -39,6 +64,12 @@ namespace myTiles {
             case "tile2":return tile2;
             case "myTile":
             case "tile1":return tile1;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
         }
         return null;
     })
